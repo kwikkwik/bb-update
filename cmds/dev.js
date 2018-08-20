9cf5e40ec6e53887f9fbe1a21a72f8fb157704d0
KidsTrap <40763827+KidsTrap@users.noreply.github.com>
2018-08-20 17:09:51 +0700

    Update dev.js

--- cmds/dev.js
+++ cmds/dev.js
@@ -15,6 +15,6 @@ exports.run = async (client, message, args, tools, map) => {
    let embed = new Discord.RichEmbed()
    .setColor("GREEN")
    .setAuthor(`${client.user.username}`,`https://cdn.discordapp.com/avatars/471150809196003328/a0ed47f2512655b5604a94e0cfb950ef.png?size=2048`)
-   .setImage(`https://www.minecraftskinstealer.com/achievement/a.php?i=3&h=Developer&t=_Brickmaster_%230218`)
+   .setImage(`https://www.minecraftskinstealer.com/achievement/a.php?i=3&h=Developer&t=Brickmaster%232000`)
    message.channel.send(embed) 
   }
